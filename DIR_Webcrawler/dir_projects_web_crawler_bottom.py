124c3567a5b41e80fa48b44709b63d1dbbb42134
damiamalfaro <damiamalfaro@gmail.com>
2024-09-14 10:26:51 -0700

    Top: 22246, Bottom: 10058. It's comming together

--- DIR_Webcrawler/dir_projects_web_crawler_bottom.py
+++ DIR_Webcrawler/dir_projects_web_crawler_bottom.py
@@ -168,7 +168,7 @@ if __name__ == '__main__':
 	# Location of storage and math associated with it
 	csv_file = 'dir_projects_bottom.csv'
 	initial_tabulation = pd.read_csv(csv_file,low_memory=False)
-	positional_location = 9697  
+	positional_location = 10058  
 
 	while positional_location != 12500:
 
