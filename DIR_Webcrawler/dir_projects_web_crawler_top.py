c4e3068a70b50eff8b43e657479affe0a4efdd71
damiamalfaro <damiamalfaro@gmail.com>
2024-09-20 23:32:03 -0700

    Top: 20063, Bottom: 12630

--- DIR_Webcrawler/dir_projects_web_crawler_top.py
+++ DIR_Webcrawler/dir_projects_web_crawler_top.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
 	'''
 	Currently, the DIR website shows a total of 540841 projects. The count of a total 27042 is below
 	'''
-	positional_location = 20118
+	positional_location = 20063
 
 	while positional_location != 12500:
 
