a5b66258aa6ca421b6bac22460491fe510bda833
damiamalfaro <damiamalfaro@gmail.com>
2024-10-13 16:51:44 -0700

    we will migrate to php slightly, especially for the display of the map

--- website/script.js
+++ website/script.js
@@ -1,6 +1,8 @@
 // Initialize the map
 var map = L.map('map').setView([37.7749, -122.4194], 5);
 
+// IMPLEMENT PHP
+
 // Add the tile layer (background map)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
