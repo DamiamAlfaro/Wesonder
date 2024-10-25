ff776c160f845b231a64865312808c804fa077ec
DamiamAlfaro <damiamalfaro@gmail.com>
2024-10-24 20:10:54 -0700

    we need to cleanse the projects display

---
+++ DIR/DIR_Projects/sorting_projects.py
@@ -0,0 +1,39 @@
+import pandas as pd  # type: ignore
+
+
+
+
+'''
+Cleanse the shit out of the csv file
+'''
+def cleansing_file(csv_file):
+
+    pass
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    # Call the faulty csv file
+    faulty_csv_file = "/Users/damiamalfaro/Desktop/Europe/testing_wesonder/Geolocations_DIR_Projects/dir_projects_faulty.csv"
+
+    # Cleanse it
+    cleansing_file(faulty_csv_file)
