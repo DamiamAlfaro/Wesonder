f0347c16bfd2013cda310b17c83e365878fdf4a8
Damiam <149447958+DamiamAlfaro@users.noreply.github.com>
2024-11-23 09:51:17 -0800

    main count: 19-2, iteration, 3195

--- Planet Bids/wesonder_planetbids_geolocations.py
+++ Planet Bids/wesonder_planetbids_geolocations.py
@@ -82,10 +82,10 @@ where they are extract, and where the entities are found.
 def reading_the_csv_files(csv_file, file_number, file_itself):
     
     # Do your thing
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file, encoding='iso-8859-1')
 
     # Row number
-    count = 0
+    count = 3195
     for index, row in df.iloc[count:].iterrows():
 
         # Locate the column headers and content
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     folder_files.sort()
 
     #File number
-    count = 51
+    count = 19
     for file_number in range(count, len(folder_files)):
         file_itself = folder_files[file_number]
         reading_the_csv_files(f'{folder}{folder_files[file_number]}', file_number, file_itself)
