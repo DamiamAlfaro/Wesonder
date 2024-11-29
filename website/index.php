1a2d7394de96b05aef700a8f2ee6fce9751cd225
DamiamAlfaro <damiamalfaro@gmail.com>
2024-11-29 14:12:26 -0800

    big changes sometimes are made by minimal steps...

--- website/index.php
+++ website/index.php
@@ -1,146 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"/>
     <meta name="author" content="Damiam Alfaro"/>
-    <meta name="description" content="The Best Entity Visualization in the construction industry"/>
+    <meta name="description" content="WESONDER Map Display"/>
     <link rel="icon" href="https://wesonder.sfo3.cdn.digitaloceanspaces.com/pictures/potential_favicon_2.png" type="image/x-icon"/>
     <link href="style.css" rel="stylesheet" type="text/css">
-    <title>WeSonder</title>
+
+    <!-- Leaflet CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+    crossorigin=""/>
+    
+    <!-- Leaflet JS -->
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+    crossorigin=""></script>
+
+    <!-- Leaflet MarkerCluster CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.Default.css" />
+
+    <!-- Leaflet MarkerCluster JS -->
+    <script src="https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js"></script>
+
+    <title>Map Display</title>
+
 </head>
 <body>
 
-    <!--    DIVISION PART:
-    THE CRUST -->
+    <?php 
 
 
-    <nav class="navbar-main">
+    ?>
 
-        <div class="navbar-leftside">
+    <div id="map"></div>
 
-            <ul>
-                <li><img src="https://wesonder.sfo3.cdn.digitaloceanspaces.com/pictures/potential_favicon_2.png" alt="WeSonder_logo" class="logo"></li>
-                <li class="logotext">WeSonder</li>
-            </ul>
- <!--            logo
-            name of the website -->
-        </div>
+    <script type="text/javascript" src="script.js"></script>
 
-        <div class="navbar-rightside">
-            <ul>
-                <li><a href="https://wesonder.com/" class="navbar-option">Home</a>
-                <li><a href="about/" class="navbar-option">About</a>
-                <li><a href="objective/" class="navbar-option">Objective</a>
-                <li><a href="contact/" class="navbar-option">Contact</a>
-                <li><a href="pricing/" class="navbar-option">Pricing</a>
-                <li><a href="/" class="navbar-option">Functionality</a>
-            </ul>
-        </div>
-    </nav>
-	<main>
-
-        <!-- A superficial, mesmerizing, and marketing-influenced description and display (picture) of the service -->
-        <div class="section">
-            <div class="crust">
-                <div class="crust-side">
-                    <div class="crust-text">Looking for a specific contractor? Where is your project located? Pinpoint it
-                    in the map and find your business partner! With WeSonder, you can allocate Subcontractors, Suppliers, General Contractors, Awarding Bodies, and any service that might benefit you and your development! <br> <br>
-                    Start your journey below!</div>
-                </div>
-                <div class="crust-side-image">
-                    <img src="https://wesonder.sfo3.cdn.digitaloceanspaces.com/pictures/california_map_testing.png" class="crust-image">
-                </div>
-            </div>
-        </div>
-
-    <!--    DIVISION PART:
-    THE LITHOSPHERE -->
-
-        <div class="feautres-general-section">
-            <div class="section">
-                <div class="features-section">
-                    <div class="features-sectionheader">WESONDER Main Features</div>
-                    <div class="features-sectionfooter">Those are the Features</div>
-                    <div class="features-sectionleft">Pin Subcontractors and leave them pinned based on account</div>
-                    <div class="features-sectionmiddle">Classify Subcontractors by Parameters</div>
-                    <div class="features-sectionright">DIR Project Locations Records (upcoming Federal Ones)</div>
-                </div>
-            </div>
-
-            <div class="section">
-                <div class="features-section">
-                    <div class="features-sectionheader">EXPLAIN EACH FEATURE AND ITS IMPORTANCE SUCCINCTLY AND CONCISELY</div>
-                    <div class="features-sectionfooter">Those are the Objectives</div>
-                    <div class="features-sectionleft">100 radius pinpointing innovation</div>
-                    <div class="features-sectionmiddle">Awarding bodies</div>
-                    <div class="features-sectionright">Email subcontractor</div>
-                </div>
-            </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-    <!--     DIVISION PART:
-    THE MANTLE -->
-
-        <!-- Everything regarding the creation of payments and accounts -->
-        <div class="section">
-            <div class="mantle">
-                <div class="mantle-side">
-                    <div class="mantle-media">
-                        Where the stripe part will go.
-                    </div>
-                </div>
-                <div class="mantle-side">
-                    <div class="mantle-text">
-                        Start the jorney now. 
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-	</main>
-
-    <!--     DIVISION PART:
-    THE CORE -->
-
-
-	<footer>
-        <div class="section">
-            <div class="core">
-                <div class="core-logo">
-                    <img src="https://wesonder.sfo3.cdn.digitaloceanspaces.com/pictures/potential_favicon_2.png" title="WeSonder Logo" class="logo">
-                    <div class="footer-logotext">
-                        <p>WeSonder</p>
-                    </div>
-                </div>
-                <div class="core-text">
-                    <ul>
-                        <li><a href="about/" class="footer-link">About</a></li>
-                        <li><a href="objective/" class="footer-link">Objective</a></li>
-                        <li><a href="features/" class="footer-link">Features</a></li>
-                        <li><a href="contact/" class="footer-link">Contact</a></li>
-                        <li><a href="pricing/" class="footer-link">Pricing</a>
-                    </ul>
-
-                </div>
-                <div class="core-damiam">
-                    <center><p class="production-owner">A production by <a href="https://www.damiamalfaro.com/about/" class="myown-websitelink">Damiam Alfaro</a></p></center>
-                    <center><p>©Copyright 2024. All rights reversed.</p></center>
-                </div>
-            </div>
-        </div>
-	</footer>
-
-	<script type="script.js"></script>
 
 </body>
 </html>
