13f8664b42adc11ef8b7bb297b4ce6b6acd67e82
Damiam <149447958+DamiamAlfaro@users.noreply.github.com>
2024-11-29 15:22:15 -0800

    home page

---
+++ website/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="author" content="Damiam Alfaro"/>
+    <meta name="description" content="WESONDER Map Display"/>
+    <link rel="icon" href="https://wesonder.sfo3.cdn.digitaloceanspaces.com/pictures/potential_favicon_2.png" type="image/x-icon"/>
+    <link href="style.css" rel="stylesheet" type="text/css">
+
+
+    <title>Home</title>
+
+</head>
+<body>
+
+    <?php 
+    
+    echo "UNDER CONSTRUCTION" . "<br>" . "-Damiam"
+
+
+    ?>
+
+    <div id="map"></div>
+
+    <script type="text/javascript" src="script.js"></script>
+
+
+</body>
+</html>
