e58ae823913855ab8b08d47a7f2004a905746eb8
Damiam <149447958+DamiamAlfaro@users.noreply.github.com>
2024-12-14 13:47:07 -0800

    we output the data to index.php accurately

--- website/map_display_projects/project_loading.php
+++ website/map_display_projects/project_loading.php
@@ -1,5 +1,4 @@
 <?php
-
 // To my understanding, the code below is extracting the response from the 'index.php' function handleCheckboxClick
 // where fetching of this file is occurring. The goal here is to first, make sure that the fetching actually works
 // with the 'if' statement below.
@@ -23,62 +22,70 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $conn = new mysqli($host, $username, $password, $dbname);
     
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } else {
-        echo "\r\nWorking Properly\r\n";
-    }
+        echo json_encode(["error" => "Connection failed: " . $conn->connect_error]);
+        exit();
+    } 
     
     $sql = "SELECT * FROM dir_projects WHERE county = '$checkboxValue' LIMIT 10";
     $result = $conn->query($sql);
     
     // Once connected to MySQL, let's implement all of the variables from the table in question, we will need them 
-    // for map display in the future.
+    // for map display in the future. We will create a list where we will store every single attribute of each of
+    // the projects we want to display, once the list is filled with the variables, we will output it using to the
+    // index file where the actual display takes place.
     
+    $projects = [];
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            
-            $project_number = $row['project_number'];
-            $project_name = $row['project_name'];
-            $awarding_body = $row['awarding_body'];
-            $project_id_number = $row['project_id_number'];
-            $project_dir_number = $row['project_dir_number'];
-            $project_description = $row['project_description'];
-            $start_date = $row['date_started'];
-            $finish_date = $row['date_finished'];
-            $complete_address = $row['complete_address'];
-            $x_coordinates = $row['x_coordinates'];
-            $y_coordinates = $row['y_coordinates'];
-            $county = $row['county'];
-            
-            $string_display = "
-            <strong>Project Name:</strong> $project_name <br>
-            <strong>Project Number:</strong> $project_number <br>
-            <strong>Awarding Body:</strong> $awarding_body <br>
-            <strong>Address:</strong> $complete_address <br>
-            <strong>Project ID Number:</strong> $project_id_number <br>
-            <strong>Project DIR Number:</strong> $project_dir_number <br>
-            <strong>Description:</strong> $project_description <br>
-            <strong>Start Date:</strong> $start_date <br>
-            <strong>End Date:</strong> $finish_date <br>
-            ";
-            
-            echo $string_display . "\r\n\r\n";
-            
+            $projects[] = [
+                "project_number" => $row['project_number'],
+                "project_name" => $row['project_name'],
+                "awarding_body" => $row['awarding_body'],
+                "project_id_number" => $row['project_id_number'],
+                "project_dir_number" => $row['project_dir_number'],
+                "project_description" => $row['project_description'],
+                "start_date" => $row['date_started'],
+                "finish_date" => $row['date_finished'],
+                "complete_address" => $row['complete_address'],
+                "x_coordinates" => $row['x_coordinates'],
+                "y_coordinates" => $row['y_coordinates'],
+                "county" => $row['county']
+            ];
         }
     }
     
+
+    if ($checkboxChecked === 'true') {
+        echo json_encode([
+            "status" => "success",
+            "projects" => $projects
+        ]);
+    } else {
+        echo json_encode([
+            "status" => "unchecked",
+            "message" => "Checkbox was unchecked."
+        ]);
+    }
+    exit();
+            
+    //         $string_display = "
+    //         <strong>Project Name:</strong> $project_name <br>
+    //         <strong>Project Number:</strong> $project_number <br>
+    //         <strong>Awarding Body:</strong> $awarding_body <br>
+    //         <strong>Address:</strong> $complete_address <br>
+    //         <strong>Project ID Number:</strong> $project_id_number <br>
+    //         <strong>Project DIR Number:</strong> $project_dir_number <br>
+    //         <strong>Description:</strong> $project_description <br>
+    //         <strong>Start Date:</strong> $start_date <br>
+    //         <strong>End Date:</strong> $finish_date <br>
+    //         ";
+            
+            
+    //     }
+    // }
     
     
 
-    // Process the checkbox data
-    if ($checkboxChecked === 'true') {
-        echo  "Checkbox $checkboxName with value $checkboxValue was checked.";
-        // Add your logic for a checked state here
-    } else {
-        echo "Checkbox $checkboxName with value $checkboxValue was unchecked.";
-        // Add your logic for an unchecked state here
-    }
-    exit;
 }
 
 ?>
