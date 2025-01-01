65af214957276e56b5fec39a93539509487b5d02
Damiam <149447958+DamiamAlfaro@users.noreply.github.com>
2025-01-01 15:48:15 -0800

    real time acquisition of bidnet direct bids completed

--- Bidnet Direct/bidnet_direct_arrangement.py
+++ Bidnet Direct/bidnet_direct_arrangement.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import sys
 import requests
 import numpy as np
+import re
 import time
 import os
+from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
@@ -180,7 +183,7 @@ def bidnet_direct_instances_allocation(csv_file,count):
 
 
 '''
-Using the following function we will store the planetbids bid attributes into a csv file
+Part of Step 3: Using the following function we will store the planetbids bid attributes into a csv file
 in order to access it later. The question is, how should the arrangement of columns be
 within the file?
 '''
@@ -209,7 +212,7 @@ def allocate_attributes_within_csv(attributes_list):
 
 
 '''
-Using Nominatim, we will try to find the addresses that were not found in the
+Part of Step 3: Using Nominatim, we will try to find the addresses that were not found in the
 first attempt, we can copy the same structure as the function above in order to
 allow this function to be used within an iteration.
 '''
@@ -236,7 +239,7 @@ def obtaining_new_geolocations_attempt2(address_string):
 
 
 '''
-Using this function, we will extract the geolocation of each of the counties found on the function
+Part of Step 3: Using this function, we will extract the geolocation of each of the counties found on the function
 below, after that, we will store the data into a csv file by the dragged variables which will 
 serve as additional datums.
 '''
@@ -387,34 +390,203 @@ def awarding_bodies_bidnet_direct_sites(csv_file):
 
 
 
+'''
+Part of Step 4: A mere integration into a csv file, nothing more, nothing less...
+'''
+def bidnet_direct_bids_into_csv(list_of_attributes):
     
+    # Just a simple allocation into the csv file. The headers of the file are the 
+    # following: AwardingBody, URL, County, X_Coordinates, Y_Coordinates, SolicitationNumber,
+    # SolicitationName, OpeningDate, ClosingDate, SolicitationURL, and ClosingTime.
+    # Adjust comment/variable name below accordingly:
+    
+    file_name = 'real_time_bidnet_direct_bids.csv'
+
+    df = pd.DataFrame({
+        "AwardingBody":[list_of_attributes[0]],
+        "URL":[list_of_attributes[1]],
+        "County":[list_of_attributes[2]],
+        "X_Coordinates":[list_of_attributes[3]],
+        "Y_Coordinates":[list_of_attributes[4]],
+        "SolicitationNumber":[list_of_attributes[5]],
+        "SolicitationName":[list_of_attributes[6]],
+        "OpeningDate":[list_of_attributes[7]],
+        "ClosingDate":[list_of_attributes[8]],
+        "SolicitationURL":[list_of_attributes[9]],
+        "ClosingTime":[list_of_attributes[10]],
+    })
+
+    if not os.path.isfile(file_name):
+        df.to_csv(file_name, index=False, header=True, mode='w')
+
+    else:
+        df.to_csv(file_name, index=False, header=False, mode='a')
+
+
+
+'''
+Part of Step 4: For neat and organization purposes, we will write an additional function of the acquisition
+of individual bidnet direct bid times from the respective href value of each of the bids from the main
+bid table of each functional bidnet url.
+'''
+def bidnet_direct_bid_time_acquisition(url):
+    
+    # Similar to the function below, we will use beautifulsoup to acquire the bid time from the webpage
+    # provided from the iteration below. We need to give it a few seconds as timeout in order to avoid
+    # any discrepancies or response errors. In the result, we have 'PM PST', which will be remain adjoint
+    # to the time in case we need it later, otherwise we can just do a time conversion, no big deal. 
+
+    response = requests.get(url, timeout=4)
+    soup = BeautifulSoup(response.content,'html.parser')
+
+    main_box = soup.find('div',class_='dateLabels')
+    div_within_main_box = main_box.find_all('div',class_='mets-field')
+    closing_time_element = div_within_main_box[2].find('div',class_='mets-field-body')
+    closing_time = " ".join(closing_time_element.text.replace('\t','').replace('\n','').split(' ')[1:])
+    
+    return closing_time
+
+
+
+
+'''
+Part of Step 4: The attempt to webscrap using Beautifulsoup. Which I like it more since we cease sessions
+and instead scrape the data right away, without opening the browser, which makes it quite fast and useful.
+In comparison with Selenium that it takes up to 10 seconds for each session to extract the data. But, let's
+try it first.
+'''
+def the_actual_bidnet_direct_webscraping(url, county, x_coord, y_coord):
+
+
+    # First we will try Beautifulsoup, a webscraping library I enjoy more than selenium due to its efficiency,
+    # speed, and reliability. The issue with this library is that we cannot do javascript functionalities such 
+    # as clicks, presses, typing, etc, solely webscraping. 
+
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'html.parser')
+
+
+    # This is a crucial step: in the following code, we will iterate through the every single attribute of the
+    # weblink and use stratification methodologies to acquire the respective variable of the bid we are looking
+    # for. Recall that stratification methodology involves the usage of <html> tags and elements layers, every
+    # single datum in the web can be acquired if you know the proper layers and <html> tags that surrond the 
+    # datum in question. We will get all four crucial variables from each bid, all attributes will be allocated 
+    # into a list solicitation_attributes_list:
+    
+    # 1) Solicitation Name and Number: Solicitation Number and Name.
+    # 2) Solicitation Dates: Opening and Closing Dates.
+    # 3) Solicitation URLs: The href value of each Solicitation.
+    # 4) Solicitation Due Time: This cannot be acquired withuth (3) Solicitation URL.
+
+    # We will also place two Except statements, the first one, AttributeError will be assigned for the respective
+    # scenarios where no bids are published from the bidnet direct site. The second one, Exception, will be 
+    # assigned for the rest of scenarios, you know, unforeseen problems. Within each iteration, we will allocate
+    # the six attributes to a csv file continously, with the append mode.
+
+    try:
+        awarding_body_name_element = soup.find('div',class_='buyerOrganizationName').text.replace('\t','').replace('\n','')
+        awarding_body_name = awarding_body_name_element.replace('Bid Opportunities','')
+
+    except:
+        awarding_body_name = 'none'
+
+    try:
+        solicitation_table = soup.find('table',class_='sol-table')
+        tbody = solicitation_table.find('tbody')
+        solicitiations = tbody.find_all('tr')
         
-   
-        
+        for each_solicitation in solicitiations[:-1]:
+
+            print('-------------------------------------------------------')
+
+            solicitation_attributes_list = [awarding_body_name, url, county, x_coord, y_coord]
+            print(f'Awarding Body: {awarding_body_name}\nURL: {url}\nCounty: {county}')
+
+            solicitation_attributes_element = each_solicitation.find('div',class_='sol-info-container')
+            solicitation_number_element = solicitation_attributes_element.find('div',class_='sol-num')
+            solicitation_number = solicitation_number_element.text.replace('\t','').replace('\n','')
+            solicitation_attributes_list.append(solicitation_number)
+            print(f'Solicitation Number: {solicitation_number}')
+
+            solicitation_name_element = solicitation_attributes_element.find('div',class_='sol-title')
+            solicitation_name = solicitation_name_element.text.replace('\t','').replace('\n','')
+            solicitation_attributes_list.append(solicitation_name)
+            print(f'Solicitation Name: {solicitation_name}')
+
+            dates_attributes = each_solicitation.find('span',class_='dates-col')
+            opening_date_element = dates_attributes.find('span',class_='sol-publication-date')
+            opening_date = opening_date_element.find('span',class_='date-value').text
+            solicitation_attributes_list.append(opening_date)
+            print(f'Opening Date: {opening_date}')
+
+            closing_date_element = dates_attributes.find('span',class_='sol-closing-date')
+            closing_date = closing_date_element.find('span',class_='date-value').text
+            solicitation_attributes_list.append(closing_date)
+            print(f'Closing Date: {closing_date}')
+
+            solicitation_link_element = solicitation_name_element.find('a').get('href')
+            solicitation_link = f'https://www.bidnetdirect.com{solicitation_link_element}'
+            solicitation_attributes_list.append(solicitation_link)
+            print(f'Solicitation Link: {solicitation_link}')
+            
+            closing_time = bidnet_direct_bid_time_acquisition(solicitation_link)
+            solicitation_attributes_list.append(closing_time)
+            print(f'Solicitation Due Time: {closing_time}')
+
+            bidnet_direct_bids_into_csv(solicitation_attributes_list)
 
             
-    
-    
-
-    
-    
 
 
+    except AttributeError as exe:
+        solicitation_attributes_list = [
+            awarding_body_name, url,county,
+            x_coord,y_coord,'none',
+            'none','none','none',
+            'none','none','none'
+        ]
+        bidnet_direct_bids_into_csv(solicitation_attributes_list)
+    
+    except Exception as exe:
+        print(f'ANOTHER MATTER\n\n{exe}')
+        sys.exit(1)
+
     
 
+
+
+
+
+
+
+
+
+
+'''
+Step 4: The real work begins. Let's webscrap based on the links from the functional file, and acquire the
+same attributes of each page accordingly.
+'''
+def bidnet_direct_real_time_webscraping(csv_file):
+    
+    # The first step is to of course, assign a selenium session... Perhaps Beautifulsoup could work here,
+    # bidnet direct seem to be non-javascript, but I might be wrong, so let's try both of them out. Of course
+    # in the course of attempts, we will figure out the variables we want to extract from each session.
+
+    df = pd.read_csv(csv_file)
+    total_rows = len(df)
+
+    for index, row in df.iterrows():
         
+        url = row['BidNetDirectUrl']
+        county = row['County']
+        x_coordinate = row['X_Coordinates']
+        y_coordinate = row['Y_Coordinates']
 
+        the_actual_bidnet_direct_webscraping(url, county, x_coordinate, y_coordinate)
 
-
-
-
+        print(f'\nIteration #{index} - Percentage Completed: {round((index/total_rows)*100,2)}%\n\n\n')
         
-
-
-
-    
-
-
 
 
 
@@ -433,6 +605,7 @@ if __name__ == "__main__":
     dir_entities_file = 'dir_entities_refined.csv' # Step 1 Input 
     awarding_bodies_file = 'awarding_body_entities.csv' # Step 2 Input - Step 1 Output
     bidnet_direct_awarding_bodies = 'bidnet_direct_awarding_bodies.csv' # Step 3 Input - Step 2 Output
+    funtional_bidnet_direct_sites = 'functional_bidnet_direct_sites.csv' # Step 4 Input - Step 3 Output
 
     step = int(input('Step: '))
 
@@ -489,6 +662,20 @@ if __name__ == "__main__":
             # 3) additional_bidnet_direct_singular_sites.csv
 
             awarding_bodies_bidnet_direct_sites(bidnet_direct_awarding_bodies)
+
+        case 4:
+
+            # Step 4 - Real Time Webscraping: Using the functional bidnet direct sites we will iterate through
+            # each and acquire the information regarding every single active bid, just how we did with the real
+            # time display of planetbids. We are going to have an allocated csv for the real-time display 
+            # accordingly.
+
+            # Files Input:
+            # 1) functional_bidnet_direct_sites.csv
+
+            bidnet_direct_real_time_webscraping(funtional_bidnet_direct_sites)
+
+
 
 
             
