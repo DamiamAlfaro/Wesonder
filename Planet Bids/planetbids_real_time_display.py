201b2260790dd74b78959125ec162b2675481e68
Damiam <149447958+DamiamAlfaro@users.noreply.github.com>
2025-01-01 17:35:53 -0800

    Multi-Browser Webscraping added

--- Planet Bids/planetbids_real_time_display.py
+++ Planet Bids/planetbids_real_time_display.py
@@ -12,65 +12,52 @@ from geopy.geocoders import Nominatim
 
 
 '''
-We will utilize the file containing all California places to correlate the title
-of the owner within each planetbids site with a county in California which we will
-later use to display by allocating a general geolocation to each of the 57 counties
-in the state. 
-'''
-def location_check(title_string):
-
-    # One thing that we need to secure is that the title_string has to be in the same
-    # string format as the california places. In this case, the csv file contains
-    # the respective places in Title form.
-
-    title_string = str(title_string).title()
-    california_places = 'california_places.csv'
-    df_cali_places = pd.read_csv(california_places,low_memory=False)
-    towns_municipalities_cities = list(df_cali_places['Location'])
-    counties = list(df_cali_places['County'])
-    combined_places = list(zip(towns_municipalities_cities, counties))
-
-
-
-
-'''
 As usual, we will allocate the attributes of each bid into a csv file for later usage.
 '''
 def attributes_into_csv(list_of_attributes):
 
-    # The headers for the csv file are the following: DatePosted, BidName, SolicitationNumber,
-    # DueDate, DueTime, SubmissionMethod, BidPlanetbidsLink, AwardingBody, and WebPageTitle.
+    # The headers for the csv file are the following: AwardingBody, PlanetbidsABLink, County,
+    # X_Coordinates, Y_Coordinates, DatePosted, BidName, SolicitationNumber, DueDate, DueTime,
+    # SubmissionMethod, and BidUrl.
 
+    file_name = 'testing_real_time_planetbids_bids.csv'
+        
     df = pd.DataFrame({
-        "DatePosted":[list_of_attributes[0]],
-        "BidName":[list_of_attributes[1]],
-        "SolicitationNumber":[list_of_attributes[2]],
-        "DueDate":[list_of_attributes[3]],
-        "DueTime":[list_of_attributes[4]],
-        "SubmissionMethod":[list_of_attributes[5]],
-        "BidPlanetbidsLink":[list_of_attributes[6]],
-        "AwardingBody":[list_of_attributes[7]],
-        "WebPageTitle":[list_of_attributes[8]]
+        "AwardingBody": [list_of_attributes[0]],
+        "PlanetbidsABLink": [list_of_attributes[1]],
+        "County": [list_of_attributes[2]],
+        "X_Coordinates": [list_of_attributes[3]],
+        "Y_Coordinates": [list_of_attributes[4]],
+        "DatePosted": [list_of_attributes[5]],
+        "BidName": [list_of_attributes[6]],
+        "SolicitationNumber": [list_of_attributes[7]],
+        "DueDate": [list_of_attributes[8]],
+        "DueTime": [list_of_attributes[9]],
+        "SubmissionMethod": [list_of_attributes[10]],
+        "BidUrl": [list_of_attributes[11]]
     })
 
-    df.to_csv('real_time_planetbids_bids.csv',index=False,header=False, mode='a')
+    if not os.path.isfile(file_name):
+        df.to_csv(file_name, index=False, header=True, mode='w')
+
+    else:
+        df.to_csv(file_name, index=False, header=False, mode='a')
 
 
 
 
 '''
-The following function is to webscrap the active projects from each planetbids
+Part of Step 1: The following function is to webscrap the active projects from each planetbids
 sites in order to display them later. The goals with the following function is
 to 1) get the name of the town/city/county associated with the weblink, and
 2) acquire the data information for the active bids within the weblink in question.
-'''
-def planetbids_active_bids_webscraping(url, awarding_body):
 
-    time.sleep(4)
-    
-    driver = webdriver.Chrome()
-    driver.get(url)
-    time.sleep(5)
+Step 1 Refinement: We will utilize multiple browsers, if the selenium session decides to fail
+with one of the browesers we will jump to the next one and attempt the execution with that
+one.
+'''
+def planetbids_active_bids_webscraping(url, awarding_body, county, x_coord, y_coord):
+
 
     # We will utilize the stratification functionality: we will enter several layes of
     # html elements in order to acquire the desired datum from each of the tabulations
@@ -79,83 +66,124 @@ def planetbids_active_bids_webscraping(url, awarding_body):
     # to acquire the unique url link for that particular bid page. This will facilitate 
     # the acquisition of bid links.
 
-    unique_planetbids_site_id_number = str(url).split('/')[4]
-    site_title = driver.find_element(By.TAG_NAME,'h4').text
+    # Refinement: We experiment with this new hypothesis of Multi-Browser Webscraping in order to
+    # remove as much possibility for a browser to fail as possible. If this works properly, we plan
+    # to also implement the Multi-Session Webscraping functionality as well, to make this progress 
+    # faster, but first it has to work and increase accuracy, otherwise I cannot extend it via 
+    # Multi-Session Webscrapong.
+
+
+    browsers = [
+        ("Chrome", lambda: webdriver.Chrome()),
+        ("Firefox", lambda: webdriver.Firefox()),
+        ("Safari", lambda: webdriver.Safari()),
+        ("Edge", lambda: webdriver.Edge())
+    ]
+
+    driver = None
+
+    for browser_name, browser_constructor in browsers:
+        try:
+            driver = browser_constructor()
+            driver.get(url)
+            time.sleep(5)
+
+
+            unique_planetbids_site_id_number = str(url).split('/')[4]
+            site_title = driver.find_element(By.TAG_NAME,'h4').text
+
+            tbody = driver.find_element(By.TAG_NAME,'tbody')
+            tr_elements = tbody.find_elements(By.TAG_NAME,'tr')
+            total_active_bids = 0
+
+            for individual_tr_element in range(len(tr_elements)):
+
+
+                # For every active bid that we find, we are going to split its text form string
+                # by empty spaces in order to acquire each information attribute about the bid.
+                # We can calculate the dates ourselves later, right now we just need the important
+                # attributes such as solicitation number, due date, due time, date posted, its link,
+                # and its submission format. We will need to break down the <tr> elements into 
+                # individual <td> elements and assign each index to a value. Like I said, stratification
+                # process. 
+
+                # List of attributes order:
+                # 1) AwardingBody
+                # 2) PlanetbidsABLink
+                # 3) County
+                # 4) X_Coordinates
+                # 5) Y_Coordinates
+                # 6) DatePosted
+                # 7) BidName
+                # 8) SolicitationNumber
+                # 9) DueDate
+                # 10) DueTime
+                # 11) SubmissionMethod
+                # 12) BidUrl
+
+
+                individual_tr_element_string = tr_elements[individual_tr_element].text
+
+                if 'Bidding' in individual_tr_element_string:
+
+                    total_active_bids += 1
+                    
+                    print(f"\nBid #{individual_tr_element}")
+
+                    list_of_attributes = [awarding_body, url, county, x_coord, y_coord]
+
+                    link_rowattribute = tr_elements[individual_tr_element].get_attribute('rowattribute')
+                    planetbids_bid_link = f'https://vendors.planetbids.com/portal/{unique_planetbids_site_id_number}/bo/bo-detail/{link_rowattribute}'
+
+                    td_elements = tr_elements[individual_tr_element].find_elements(By.TAG_NAME,'td')
+                    date_posted = td_elements[0].text
+                    list_of_attributes.append(date_posted)
+                    print(f"Date Posted: {date_posted}")
+
+                    bid_name = td_elements[1].text
+                    list_of_attributes.append(bid_name)
+                    print(f"Bid Name: {bid_name}")
+
+                    solicitation_number = td_elements[2].text
+                    list_of_attributes.append(solicitation_number)
+                    print(f"Solicitation Number: {solicitation_number}")
+
+                    due_date = td_elements[3].text.split(" ")[0]
+                    list_of_attributes.append(due_date)
+                    print(f"Due Date: {due_date}")
+
+                    due_time = td_elements[3].text.split(" ")[1]
+                    list_of_attributes.append(due_time)
+                    print(f"Due Time: {due_time}")
+
+                    submission_method = td_elements[6].text
+                    list_of_attributes.append(submission_method)
+                    print(f"Submission Method: {submission_method}")
+
+                    list_of_attributes.append(planetbids_bid_link)
+                    print(f'Bid Link: {planetbids_bid_link}')
+
+                    attributes_into_csv(list_of_attributes)
+
+                else:
+                    pass
+
+            print(f'\n{awarding_body} Total Active Bids = {total_active_bids}')
+            break
     
-    
-    #location_check(site_title)
+        except Exception as exe:
+            print(f"Error with {browser_name}: {exe}")
+            if driver:
+                driver.quit()
+            continue 
 
-    tbody = driver.find_element(By.TAG_NAME,'tbody')
-    tr_elements = tbody.find_elements(By.TAG_NAME,'tr')
-    total_active_bids = 0
+    if driver:
+        driver.quit()
+        print(f"Finished scraping with {browser_name}.")
 
-    for individual_tr_element in range(len(tr_elements)):
+    else:
+        print("All browser attempts failed.")
 
-        # For every active bid that we find, we are going to split its text form string
-        # by empty spaces in order to acquire each information attribute about the bid.
-        # We can calculate the dates ourselves later, right now we just need the important
-        # attributes such as solicitation number, due date, due time, date posted, its link,
-        # and its submission format. We will need to break down the <tr> elements into 
-        # individual <td> elements and assign each index to a value. Like I said, stratification
-        # process. 
-
-        individual_tr_element_string = tr_elements[individual_tr_element].text
-
-        if 'Bidding' in individual_tr_element_string:
-
-            total_active_bids += 1
-            
-            print(f"Bid #{individual_tr_element}")
-
-            list_of_attributes = []
-
-            link_rowattribute = tr_elements[individual_tr_element].get_attribute('rowattribute')
-            planetbids_bid_link = f'https://vendors.planetbids.com/portal/{unique_planetbids_site_id_number}/bo/bo-detail/{link_rowattribute}'
-
-            td_elements = tr_elements[individual_tr_element].find_elements(By.TAG_NAME,'td')
-            date_posted = td_elements[0].text
-            list_of_attributes.append(date_posted)
-            print(f"Date Posted: {date_posted}")
-
-            bid_name = td_elements[1].text
-            list_of_attributes.append(bid_name)
-            print(f"Bid Name: {bid_name}")
-
-            solicitation_number = td_elements[2].text
-            list_of_attributes.append(solicitation_number)
-            print(f"Solicitation Number: {solicitation_number}")
-
-            due_date = td_elements[3].text.split(" ")[0]
-            list_of_attributes.append(due_date)
-            print(f"Due Date: {due_date}")
-
-            due_time = td_elements[3].text.split(" ")[1]
-            list_of_attributes.append(due_time)
-            print(f"Due Time: {due_time}")
-
-            submission_method = td_elements[6].text
-            list_of_attributes.append(submission_method)
-            print(f"Submission Method: {submission_method}")
-
-            list_of_attributes.append(planetbids_bid_link)
-            print(f'Bid Link: {planetbids_bid_link}')
-
-            list_of_attributes.append(awarding_body)
-            print(f'Awarding Body: {awarding_body}')
-
-            list_of_attributes.append(site_title)
-            print(f'Bid Title: {site_title}\n')
-
-
-
-            attributes_into_csv(list_of_attributes)
-
-            
-
-        else:
-            pass
-
-    print(f'\n{awarding_body} Total Active Bids = {total_active_bids}')
 
 
 
@@ -172,21 +200,32 @@ def planetbids_sites_iteration(csv_file, count):
     # for each we will have a different function associated with it.
 
     df = pd.read_csv(csv_file,low_memory=False)
+    total_rows = len(df)
 
-
-    for index, row in df.iloc[count:].iterrows():
+    for index, row in df.iloc[count:].head(5).iterrows():
         
         awarding_body = row['AwardingBody']
         weblink = row['WebLink']
-        print(f'Current Iteration: {index}')
+        county = row['County']
+        x_coord = row['X_Coordinates']
+        y_coord = row['Y_Coordinates']
 
-        try:
-            planetbids_active_bids_webscraping(weblink, awarding_body)
-            percentage_completed = round((index/len(df)) * 100, 2)
-            print(f'\nPercentage Completed: {percentage_completed}%\n')
+        planetbids_active_bids_webscraping(
+            url=weblink, 
+            awarding_body=awarding_body, 
+            county=county, 
+            x_coord=x_coord, 
+            y_coord=y_coord
+        )
 
-        except:
-            print(f'\n{awarding_body}: None\n')
+        print(f'Iteration #{index} just Completed - Percentage: {round((index/total_rows)*100,2)}%\n')
+        print('-------------------------------------------------------')
+
+        
+        
+        
+
+
 
 
 
@@ -391,24 +430,6 @@ def planetbids_site_county_and_geolocation(planetbids_sites, count):
             faulty_county_of_bid(url, awarding_body)
             
 
-        
-        
-
-
-
-
-    
-
-
-        
-        
-
-
-
-
-    
-
-
 
 
 
@@ -427,7 +448,7 @@ if __name__ == "__main__":
 
     planetbids_sites_csv_file = 'planetbids_sites.csv' # Step 1 Input
     active_bids_planetbids = 'real_time_planetbids_bids.csv' # Step 4 Input - Step 1 Output
-    refined_planetbids_sites = 'refined_planetbids_sites.csv' # Step 3 Input - Step 2 Output
+    refined_planetbids_sites = 'refined_planetbids_sites.csv' # Step 3 & 1 Input - Step 2 Output
     faulty_planetbids_sites = 'faulty_planetbids_sites.csv' # Step 3 Input - Step 2 Output
     
 
@@ -437,6 +458,7 @@ if __name__ == "__main__":
 
         case 1:
 
+
             # Step 1 - Real Time Bids: The one and only step to do here is to iterate through each of the planetbids sites,
             # scroll a few scrolls down in order to acquire all of the active bids, collect their
             # date information, acquire the geolocation of the respective city on planetbids 
@@ -444,16 +466,19 @@ if __name__ == "__main__":
             # store it into a csv file with geolocations in order to be quickly displayed. The 
             # following function will have several more functions in usage as well.
 
-            # Files Input:
-            # 1) planetbids_sites.csv
+            # Files Input (Refinement):
+            # 1) refined_planetbids_sites.csv
 
             # Files Output:
             # 1) real_time_planetbids_bids.csv
 
+
             count = int(input('Count: '))
-            planetbids_sites_iteration(planetbids_sites_csv_file, count)
+            planetbids_sites_iteration(refined_planetbids_sites, count)
+
 
         case 2:
+
 
             # Step 2 - Department Locations: We changed our minds, instead of using the title webpage string as
             # an indicator for location of the real time planetbids bid, we will first iterate through all of 
@@ -468,10 +493,13 @@ if __name__ == "__main__":
             # 1) refined_planetbids_sites.csv
             # 2) faulty_planetbids_sites.csv
 
+
             count = int(input('Count: '))
             planetbids_site_county_and_geolocation(planetbids_sites_csv_file, count, step)
 
+
         case 3:
+
 
             # Step 3 - Regurgitation Iteration: One of the output files from the function above is a two-column
             # csv file containing the planetbids urls that were faulty in the previous session, let's try to 
@@ -488,11 +516,5 @@ if __name__ == "__main__":
             count = int(input('Count: '))
             planetbids_site_county_and_geolocation(faulty_planetbids_sites, count)
 
-        case 4:
 
-            # Step 4 - Discrepancy Correlation: This isn't really a step, but it is necessary nonentheless. We will
-            # check to see if all of the real-time bids have a correlation to at least one of the awarding bodies from
-            # the refined_planetbids_sites.csv from Step 2 is found the respective real-time column of awarding body of 
-            # the file. Basically, to check that all real-time bids are correlated between files via AwardingBody column.
-
-            pass
+        
