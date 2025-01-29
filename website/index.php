a44e7a85f32d582f3079dbf145b2a0c4667a8994
DamiamAlfaro <damiamalfaro@gmail.com>
2025-01-28 20:54:00 -0800

    video embedded

--- website/index.php
+++ website/index.php
@@ -2,27 +2,29 @@
 <html>
 <head>
     <meta charset="UTF-8"/>
-    <meta name="author" content="Damiam Alfaro"/>
+    <meta name="author" content="Wesonder"/>
     <meta name="description" content="WESONDER Map Display"/>
-    <link rel="icon" href="https://wesonder.sfo3.cdn.digitaloceanspaces.com/pictures/potential_favicon_2.png" type="image/x-icon"/>
+    <link rel="icon" href="pictures/" type="image/x-icon"/>
     <link href="style.css" rel="stylesheet" type="text/css">
 
 
-    <title>Home</title>
+    <title>Wesonder</title>
 
 </head>
 <body>
 
     <?php 
     
-    echo "UNDER CONSTRUCTION" . "<br>" . "-Damiam"
-
 
     ?>
 
-    <div id="map"></div>
+    <h1></h1>
+    <section id="california_video">
+        <video autoplay loop muted>
+            <source src="media/WesonderRefined.mp4" type="video/mp4">
+        </video>
 
-    <script type="text/javascript" src="script.js"></script>
+    </section>
 
 
 </body>
