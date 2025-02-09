e3c2befeef76a9d09652993ba80109b72cd32625
DamiamAlfaro <damiamalfaro@gmail.com>
2025-02-09 06:42:02 -0800

    this looks amazing, manual done

--- website/maps/projects/index.php
+++ website/maps/projects/index.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.Default.css" />
 
-    <!-- Leaflet MarkerCluster JS -->
+    <!-- Leaflet MarkerCluster JS right -->
     <script src="https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js"></script>
 
     <title>Projects</title>
