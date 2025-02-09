6274f0b95550ece882fadaafe4f5fd4724a14133
DamiamAlfaro <damiamalfaro@gmail.com>
2025-02-09 03:11:32 -0800

    bidnetdirect tabulation works

--- website/maps/bidnetdirect/index.php
+++ website/maps/bidnetdirect/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8"/>
     <meta name="author" content="Damiam Alfaro"/>
     <meta name="description" content="WESONDER Map Display"/>
-    <link rel="icon" type="image/png" href="../media/bauhaus_logo_transparent.png"/>
+    <link rel="icon" type="image/png" href="../../media/bauhaus_logo_transparent.png"/>
     <link href="../style.css" rel="stylesheet" type="text/css">
 
     <!-- Leaflet CSS -->
